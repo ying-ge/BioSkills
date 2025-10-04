a26bfd2e3896333d49ff01c59cab33bba66bdc29
Ying Ge <45988974+ying-ge@users.noreply.github.com>
2025-10-04 21:52:31 +0800

    Create collect_library.sh

---
+++ .github/scripts/collect_library.sh
@@ -0,0 +1,11 @@
+grep 'library(' FigureYa*/*.Rmd > library.txt
+sed 's/ //g' library.txt | sed 's/"//g' | sed 's/\// /g' | sed 's/(/ /g' | sed 's/)/ /g' | awk '{print $1,$3}' | sort -k 2| sort -k 1 | uniq > FigureYa_library.txt
+cut -d' ' -f2 FigureYa_library.txt | sort | uniq > library2download.txt
+
+# github from install_dependencies.R 
+grep 'https://github.com/' FigureYa*/install_dependencies.R | sed -E 's/.*(https:\/\/.*)/\1/' > github_library_R.txt
+
+# github from .Rmd
+grep 'https://github.com/' FigureYa*/*.Rmd | grep -oE 'https?://[^ ]+' | sed 's/[)>].*//g' | sed 's/[一-龥]//g' | sort | uniq > github_library_rmd.txt
+
+cat github_library_R.txt github_library_rmd.txt | cut -d'/' -f1-5 > github_library.txt
